4458c9a78137f03618bb84a84776042719f029e8
4yop <520250ss@gmail.com>
2020-01-30 18:29:38 +0800

    lq

---
+++ leetcode/question/3.无重复字符的最长子串.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+
+
+class Solution {
+
+
+    function lengthOfLongestSubstring($s) {
+
+        $len = strlen($s);
+
+        $str = '';
+        $count = 0;
+        for($i =0;$i<$len;$i++){
+            $index = strpos($str,$s[$i]);
+            if($index !== false){
+                $str = substr($str,$index+1);
+            }
+            $str .= $s[$i];
+            $count = strlen($str) > $count ? strlen($str) : $count;
+        }
+        return $count;
+        
+    }
+}
+
+$so = new Solution();
+$nums = "dvdf";
+$val = 3;
+$res = $so->lengthOfLongestSubstring($nums);
+print_r($res);
