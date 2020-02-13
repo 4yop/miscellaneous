a7d371e932a7e5a83ad78706dbcf0fca9b3c971a
4yop <520250ss@gmail.com>
2020-02-13 20:04:22 +0800

    hgsadfklsadflkads

---
+++ leetcode/question/34.在排序数组中查找元素的第一个和最后一个位置.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+
+
+class Solution {
+
+
+    function searchRange($nums, $target) {
+
+    }
+}
+
+$so = new Solution();
+$nums = array(2,2,3,3);
+$val = 3;
+$res = $so->removeElement($nums,$val);
+print_r($res);
--- leetcode/question/55. 跳跃游戏.php
+++ leetcode/question/55. 跳跃游戏.php
@@ -9,13 +9,26 @@ class Solution {
 
 
     function canJump($nums) {
-
-
-
+        if(!in_array(0,$nums)){
+            return true;
+        }
+        $queue = [ [0,$nums[0]]];
+        $end = count($nums) - 1;
+        while(!empty($queue)){
+            list($key,$val) = array_shift($queue);
+            if($key == $end){
+                return true;
+            }
+            $max = min($end,$key+$val);
+            for($i = $key + 1;$i<= $max;$i++){
+                $queue[] = [$i,$nums[$i]];
+            }
+        }
+        return false;
     }
 }
 
 $so = new Solution();
-$nums = [2,3,1,1,4];
+$nums = [3,2,1,0,4];
 $res = $so->canJump($nums);
-print_r($res);
+var_dump($res);
