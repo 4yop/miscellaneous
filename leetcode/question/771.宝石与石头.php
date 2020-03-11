ea713d8ae7980e055209d2ea58720936164b49a4
4yop <520250ss@gmail.com>
2020-03-11 17:19:07 +0800

    dfg drhghdf zxghfgh dfhgdfg

---
+++ leetcode/question/771.宝石与石头.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Solution {
+
+
+    function numJewelsInStones($J, $S) {
+        if(empty($J)) return 0;
+        $hash = array_flip(str_split($J));
+        $res = 0;
+        $strlen = strlen($S);
+        for($i = 0;$i < $strlen;$i++){
+            if(array_key_exists($S[$i],$hash)){
+                $res++;
+                while($S[$i + 1] == $S[$i] && $i < $strlen){
+                    $i++;
+                    $res++;
+                }
+            }
+        }
+        return $res;
+    }
+}
+
+
+
+
+
+
+$solution = new Solution();
+
+$res = $solution->numJewelsInStones("aA","aAAbbbb");
+print_r($res);
