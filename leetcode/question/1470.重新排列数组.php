a17e4bcfa8fc23ee6f065a2a08b7e43126cc2ef3
lzh <520250ss@gmail.com>
2020-06-23 10:33:54 +0800

    12

---
+++ leetcode/question/1470.重新排列数组.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $n
+     * @return Integer[]
+     */
+    function shuffle($nums, $n) {
+        $res = [];
+
+
+
+    }
+}
+$so = new Solution();
+
+$res = $so->shuffle( [2,5,1,3,4,7],3);
+print_r($res);
