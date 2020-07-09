1157dbc0b2a2328e84bf74e04904b130584fe619
4yop <520250ss@gmail.com>
2020-07-09 22:08:07 +0800

    1212

---
+++ leetcode/question/160.相交链表.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once "../../learn/data/linked-list.php";
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $headA
+     * @param ListNode $headB
+     * @return ListNode
+     */
+    function getIntersectionNode($headA, $headB) {
+
+
+
+
+        $A = $headA;
+        $B = $headB;
+
+        while($A != $B){
+            echo "A:{$A->val},B:{$B->val}\n";
+            $A = $A == null ? $headB : $A->next;
+            $B = $B == null ? $headA : $B->next;
+        }
+
+        return $A;
+    }
+}
+
+$arr =  [4,1,8,4,5];
+$headA = (new LinkedList())->create($arr);
+
+$arr =  [5,0,1,8,4,5];
+$headB = (new LinkedList())->create($arr);
+
+$res = (new Solution())->getIntersectionNode($headA, $headB);
+var_dump($res);
+
