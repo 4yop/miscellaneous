9155d98a37f154b3a3517e1610aa6a887ee88067
lzh <520250ss@gmail.com>
2020-07-10 09:08:11 +0800

    123

--- leetcode/question/62.不同路径.php
+++ leetcode/question/62.不同路径.php
@@ -1,5 +1,5 @@
 <?php
-//55. 跳跃游戏
+
 
 
 
@@ -8,27 +8,36 @@
 class Solution {
 
 
-    function canJump($nums) {
-        if(!in_array(0,$nums)){
-            return true;
-        }
-        $queue = [ [0,$nums[0]]];
-        $end = count($nums) - 1;
-        while(!empty($queue)){
-            list($key,$val) = array_shift($queue);
-            if($key == $end){
-                return true;
+    function uniquePaths($m, $n) {
+        //高为 n ，宽为m
+//        $x = 1;
+//        $y = 1;
+        //[3,2]
+        $queue = [
+           [1,1],
+        ];
+        $i = 0;
+        while($i < count($queue)){
+            list($x,$y) = $queue[$i];
+            if($x == $m && $y == $n){
+                $i++;
+                continue;
             }
-            $max = min($end,$key+$val);
-            for($i = $key + 1;$i<= $max;$i++){
-                $queue[] = [$i,$nums[$i]];
+            $flag = true;
+            if($x < $m){
+                $flag = false;
+
+                $queue[] = [$x+1,$y];
+            }
+            if($y < $n){
+                $queue[] = [$x+1,$y];
             }
         }
-        return false;
+
+        return $queue;
     }
 }
 
 $so = new Solution();
-$nums = [3,2,1,0,4];
-$res = $so->canJump($nums);
-var_dump($res);
+$res = $so->uniquePaths(7,3);
+print_r($res);
