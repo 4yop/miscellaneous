85be4a3bb5adcc0f599a189f1c2d576544417f5f
lzh4yop <13058377106@163.com>
2020-12-25 23:06:23 +0800

    'xx'

---
+++ recite/RedisWithReentrantLock.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class RedisWithReentrantLock
+{
+    private $lockers = null;
+
+    private $redis = null;
+    public function __construct()
+    {
+        $this->lockers = new \Thread();
+        $this->redis = new \Redis();
+        $this->redis->connect('127.0.0.1','6379');
+    }
+
+    private function _lock(string $key):bool
+    {
+        return $this->redis->set($key,"",array('nx', 'ex' => 5));
+    }
+
+    private function _unlock(string $key):void {
+        $this->redis->del($key);
+    }
+
+    private function currentLockers() {
+
+        $refs = \Thread::getCurrentThread ();
+
+        if ($refs != null)
+        {
+
+        }
+
+    }
+
+
+}
