d03086560ff385fe044a60c61b1bb2c8bbc2ac82
lzh <1131559748@qq.com>
2021-03-31 12:19:20 +0800

    sdf sdf sdf sdf

--- leetcode/question/90.子集II.php
+++ leetcode/question/90.子集II.php
@@ -2,19 +2,38 @@
 
 class Solution {
 
+    protected $result = [];
     /**
      * @param Integer[] $nums
      * @return Integer[][]
      */
     function subsetsWithDup($nums) {
-        $res = [[]];
-        for($i = 1;$i <= count($nums);$i++)
-        {
-            
+        sort($nums);
+        $this->sub($nums, [], 0);
+        $this->result[] = [];
+        return $this->result;
+    }
+
+
+    private function sub($nums, $list, $start)
+    {
+        if (count($list) == count($nums)) {
+            return;
+        }
+        for ($i = $start; $i < count($nums); ++$i) {
+            if (array_key_exists($i,$list))
+            {
+                continue;
+            }
+            $list[$i] = $nums[$i];
+            $key = implode(',',$list);
+            $this->result[$key] = $list;
+            $this->sub($nums, $list, $i + 1);
+            array_pop($list);
         }
     }
 }
-$nums = [1,2,2];
+$nums = [0];
 
 $s = new Solution();
 
