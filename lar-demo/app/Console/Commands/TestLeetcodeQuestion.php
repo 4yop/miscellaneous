f1436575cc491d20334be58f6687fe63a54208da
lzh <1131559748@qq.com>
2021-04-17 17:23:57 +0800

    1234

--- lar-demo/app/Console/Commands/TestLeetcodeQuestion.php
+++ lar-demo/app/Console/Commands/TestLeetcodeQuestion.php
@@ -21,6 +21,8 @@ class TestLeetcodeQuestion extends Command
      */
     protected $description = '测试运行leetcode题目';
 
+    private $number = '';
+    private $path = '';
     /**
      * Create a new command instance.
      *
@@ -38,11 +40,38 @@ class TestLeetcodeQuestion extends Command
      */
     public function handle()
     {
+        //echo base_path();
+
         $no = (int)$this->argument('no');
         if ($no < 1)
         {
             exit( "参数有误\n");
         }
+        $this->path = base_path('leetcode/question/');
+        $this->number = $no;
+        $this->exec();
         return 0;
+    }
+
+
+    public function exec()
+    {
+        $files = scandir($this->path);
+        foreach ($files as $file)
+        {
+            if (!is_file($this->path.$file))
+            {
+                continue;
+            }
+            $number =  (int)explode('.',pathinfo($file,PATHINFO_FILENAME))[0];
+            if ($number == $this->number)
+            {
+//                echo $this->path.$file."\n";
+                echo "\n--------leetCode question {$file}--------\n";
+                include $this->path.$file;
+                return;
+            }
+        }
+        echo "\n leetcode 没这个编号的 {$this->number}\n";
     }
 }
