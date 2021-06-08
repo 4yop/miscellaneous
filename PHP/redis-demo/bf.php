0c67e629f581f460a12fc9da1be5b59e9c0443ba
lzh <1131559748@qq.com>
2021-06-08 11:11:17 +0800

    123

--- PHP/redis-demo/bf.php
+++ PHP/redis-demo/bf.php
@@ -9,12 +9,13 @@ $arr = range(1,100000);
 
 foreach ($arr as $i=>$item)
 {
+    echo $i;
     $redis->rawCommand('bf.add','test-bf',"user_{$i}");
 
     $ret = $redis->rawCommand("bf.exists", 'test-bf', "user_{$i}");
     if ($ret == 0)
     {
-        echo "{$i}\n";
+        //echo "{$i}\n";
         break;
     }
 }
