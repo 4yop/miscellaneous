d41bf5e7d835006d2298738b28ac3c5e611db2e9
lzh <1131559748@qq.com>
2021-06-09 14:18:03 +0800

    12334

--- PHP/redis-demo/geoDemo.php
+++ PHP/redis-demo/geoDemo.php
@@ -9,14 +9,13 @@
     $redis = redis();
     $faker = Faker\Factory::create('zh_CN');
 
-    $end = 100;
 
-    $users = [];
 
     $redis->del('near_user');
 
+    $users = [];
     //随机生成用户信息 并插入redis geo
-    for($i = 0;$i < $end; $i++)
+    for($i = 0;$i < 100; $i++)
     {
         $users[$i] = [
             'id' => $i+1,
@@ -40,7 +39,7 @@
 
     $user_id = random_int(1,100);
     $position = $users[$user_id - 1]['position'];
-    //最近的50个用户
+    //最近的50个用户 距离 10000000km以内
     $res = $redis->georadius('near_user',$position['longitude'],$position['latitude'],'10000000','km',['WITHDIST','count'=>50,'ASC']);
 
     echo "当前id为:{$user_id},位置：{$position['longitude']},{$position['latitude']}\n";
