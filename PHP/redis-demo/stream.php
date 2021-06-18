8c6ef88050ce6ae17838e0ba81102e821a93f0ea
lzh <1131559748@qq.com>
2021-06-18 14:28:11 +0800

    'xx'

--- PHP/redis-demo/stream.php
+++ PHP/redis-demo/stream.php
@@ -4,8 +4,20 @@
 
     $redis = redis();
 
+    $key = "test-stream";
 
-    $msgId = $redis->xAdd('test-stream','*',['a'=>'a','b'=>'b'],10);
-    var_dump($msgId);
-    $msgId = $redis->xAdd('test-stream','*',['c'=>'c','d'=>'d'],10);
-    var_dump($msgId);
+    for($i = 0;$i < 100;$i++)
+    {
+        $msg_id = $redis->xAdd($key, '*', ['field-'.$i => 'value:'.$i]);
+    }
+
+    //删除
+    $xDelResult = $redis->xDel($key, ['1609131229884-0']);
+    //创建消费队列
+    $xGroupResult = $redis->xGroup('CREATE', $key, $key.':group_1', 0);
+    //获取消费队列中的消息
+    $xReadGroupResult = $redis->xReadGroup($key.':group_1', 'consumerA', [$key => '>'], 1);
+    //从消费者组内读取消息并处理完成后,需确认该条消息已处理
+    $xAckResult = $redis->xAck($key, $key.':group_1', ['1609131229885-0']);
+    // 用来获消费组或消费内消费者的未处理完毕的消息
+    $pendingResult = $redis->xPending($key, $key.':group_1');
