ab14f147b245d9c037f5b8cebf2fceb5bf01e617
lzh <1131559748@qq.com>
2021-07-01 10:38:05 +0800

    'xdsd'

---
+++ bat脚本/一键打开要用的.bat
@@ -0,0 +1,9 @@
+@echo off
+start /d "D:\Program Files\JetBrains\PhpStorm 2020.3.1\bin\" phpstorm64.exe
+start /d "C:\Users\Administrator\AppData\Local\SourceTree\" SourceTree.exe
+start /d "D:\phpstudy_pro\COM\" phpstudy_pro.exe
+start /d "D:\Navicat for MySQL\" navicat.exe
+start /d "D:\WeChat\" WeChat.exe
+start /d "D:\phpstudy_pro\new-lzh\lemonyyshop\GatewayWorker" start_for_win.bat
+start /d "D:\phpstudy_pro\Extensions\redis5.0.10"  redis-server.exe
+exit
