85960a6cc852539abe4c5f9b9038c0a2a4ae4d4b
lzh <1131559748@qq.com>
2021-07-15 11:42:34 +0800

    dfglkshgsdlkf

---
+++ lar-demo/leetcode/question/700.二叉搜索树中的搜索.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $val
+     * @return TreeNode
+     */
+    function searchBST($root, $val) {
+        if ($root == null)
+        {
+            return null;
+        }
+        if ($root->val === $val)
+        {
+            return $root;
+        }
+        if ($root->val > $val)
+        {
+            return $this->searchBST($root->left,$val);
+        }else
+        {
+            return $this->searchBST($root->right,$val);
+        }
+    }
+}
--- redis/SDS.php
+++ redis/SDS.php
@@ -1,7 +1,11 @@
 <?php
 
 
-class SDS
+class Sdshdr
 {
+    public int $free = 0; // buf[]数组未使用字节的数量
 
+    public int $len = 0; // buf[]数组所保存的字符串的长度
+
+    public array $buf = []; // 保存字符串的数组
 }
