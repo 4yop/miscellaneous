7390ab23432227bc1484f922fec8669cf0f5eefc
lzh <1131559748@qq.com>
2021-07-23 18:12:59 +0800

    dfglkshgsdlkf

--- NetworkProtocol/TCP.php
+++ NetworkProtocol/TCP.php
@@ -1,12 +1,23 @@
 <?php
 
 
-class TCP
+global $net_secret;
+$net_secret = [];
+function secure_tcp_sequence_number($saddr,$daddr,$sport,$dport)
 {
-    public $name = '传输控制协议';
-
-    public $description = "面向连接,可靠的,基于字节流,全双工的传输层协议";
-
-
-
+    global $net_secret;
+    $hash = [];
+    net_secret_init();
+    $hash[0] = $saddr;
+    $hash[1] = $daddr;
+    $hash[2] = ($sport << 16) + $dport;
+    $hash[3] = $net_secret[15];
+    return seq_scale($hash[0]);
 }
+function net_secret_init(){}
+function seq_scale($seq)
+{
+    return $seq + (ktime_to_ns(ktime_get_real()) >> 6);
+}
+function ktime_get_real(){}
+function ktime_to_ns($time){}
