4718a7368b81a66a8ffada9d20d0b9b257bd18f7
lzh <1131559748@qq.com>
2021-07-24 10:16:41 +0800

    'dfglkshgsdlkf'

--- git-pull.php
+++ git-pull.php
@@ -3,7 +3,9 @@ $time = 100;
 $sec = 1;
 while (true) {
     echo "第".$sec++."次执行".date('Y-m-d H:i:s')."\n";
+    echo "\n\n-------------------------------------------------------------------------\n";
     $command = "git add -A && git commit -m 'dfglkshgsdlkf' && git push";
     exec($command);
+    echo "-------------------------------------------------------------------------\n\n";
     sleep($time);
 }
