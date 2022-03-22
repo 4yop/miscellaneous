aa6fcf5a63faea85f3efb9da91ac693dddebc5de
lzh <1131559748@qq.com>
2022-03-22 12:16:38 +0800

    x

---
+++ lar-demo/leetcode/question/844.比较含退格的字符串.php
@@ -0,0 +1,13 @@
+class Solution {
+
+/**
+ * @param String $s
+ * @param String $t
+ * @return Boolean
+ */
+function backspaceCompare($s, $t) {
+    if ($s === $t) return true; 
+    $s1 = str_split($s);
+    $t1 = str_split($t);
+}
+}
