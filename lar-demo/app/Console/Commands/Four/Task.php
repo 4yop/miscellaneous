2c06254904b331c1535b8a4837198125574671ea
lzh4yop <1131559748@qq.com>
2022-03-31 20:53:56 +0800

    6bcbbf648395adde7638738e00a9e3c5

--- lar-demo/app/Console/Commands/Four/Task.php
+++ lar-demo/app/Console/Commands/Four/Task.php
@@ -4,6 +4,11 @@ namespace App\Console\Commands\Four;
 
 use App\Service\RabbitMQ;
 use Illuminate\Console\Command;
+use PhpAmqpLib\Exception\AMQPChannelClosedException;
+use PhpAmqpLib\Exception\AMQPConnectionBlockedException;
+use PhpAmqpLib\Exception\AMQPConnectionClosedException;
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Wire\AMQPTable;
 
 class Task extends Command
 {
@@ -40,6 +45,39 @@ class Task extends Command
     public function handle()
     {
         $channel = RabbitMQ::getChannel();
+
+        /**
+         *
+         *
+         * @param string $queue 队列名
+         * @param bool $passive 是否被动 ,true的话，没队列不会自动创建
+         * @param bool $durable 是否持久化
+         * @param bool $exclusive 是否排他
+         * @param bool $auto_delete 是否自动删除
+         * @param bool $nowait 是否异步
+         * @param array|AMQPTable $arguments
+         * @param int|null $ticket
+         * @return array|null
+         */
+        $channel->queue_declare(self::$queue_name,false,true,false,false,false);
+
+        while ($input = fgets(STDIN))
+        {
+            $msg = new AMQPMessage($input,['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+            /**
+             * Publishes a message
+             *
+             * @param AMQPMessage $msg 消息体
+             * @param string $exchange 交换机
+             * @param string $routing_key 路由key
+             * @param bool $mandatory
+             * @param bool $immediate
+             * @param int|null $ticket
+             */
+            $channel->basic_publish($msg,"",self::$queue_name);
+        }
+
+
 
         return 0;
     }
