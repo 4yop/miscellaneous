ef3cbd691da40b94b49e674bbfe5bc55d22a8476
lzh <1131559748@qq.com>
2022-03-31 12:01:54 +0800

    12311313

---
+++ lar-demo/app/Service/RabbitMQ.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class RabbitMQ
+{
+    private static $connection;
+
+    public static function getConnection():AMQPStreamConnection
+    {
+        if (!self::$connection instanceof AMQPStreamConnection)
+        {
+            self::$connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', 'admin');
+        }
+        return self::$connection;
+    }
+
+    public static function getChannel():AMQPChannel
+    {
+        return self::getConnection()->channel();
+    }
+}
