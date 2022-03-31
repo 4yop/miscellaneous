22ef0e7c9db3526242b22909c6e610e962057d26
lzh4yop <1131559748@qq.com>
2022-03-31 20:59:47 +0800

    2a04f508ffe6b66ca66f6a7c775d296a

--- lar-demo/app/Console/Commands/Four/Worker.php
+++ lar-demo/app/Console/Commands/Four/Worker.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\Four;
 
+use App\Service\RabbitMQ;
 use Illuminate\Console\Command;
 
 class Worker extends Command
@@ -38,6 +39,27 @@ class Worker extends Command
      */
     public function handle()
     {
+        $channel = RabbitMQ::getChannel();
+        $callback = function () {
+
+        };
+        /**
+         * @param string $queue 队列名
+         * @param string $consumer_tag 消费者标签
+         * @param bool $no_local 这个功能属于AMQP的标准,但是rabbitMQ并没有做实现.
+         * @param bool $no_ack 是否开启自动应答
+         * @param bool $exclusive 排他
+         * @param bool $nowait 异步
+         * @param callable|null $callback 回调
+         * @param int|null $ticket
+         * @param \PhpAmqpLib\Wire\AMQPTable|array $arguments
+         */
+        $channel->basic_consume(self::$queue_name,"",false,false,false,false,);
+        while($channel->is_open())
+        {
+            $channel->wait();
+        }
+
         return 0;
     }
 }
