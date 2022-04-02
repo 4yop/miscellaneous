efd57cd27d5c3fa4766dd1fb48be790ebd05938d
lzh <1131559748@qq.com>
2022-04-02 12:18:29 +0800

    50e4f45e4b26bb678a8834bb683c996d

--- lar-demo/app/Console/Commands/Eight/Worker.php
+++ lar-demo/app/Console/Commands/Eight/Worker.php
@@ -83,9 +83,9 @@ class Worker extends Command
          */
 
         $arguments = [
-            'x-dead-letter-exchange'     => self::DEAD_EXCHANGE,//死信交换机
+            'x-dead-letter-exchange'    => self::DEAD_EXCHANGE,//死信交换机
             'x-dead-letter-routing-key' => 'lisi',//死信路由key
-            'x-message-ttl' => 'lisi',//死信路由key
+            //'x-message-ttl'             => 10000,//过期时间 1000毫秒=1秒
         ];
         $channel->queue_declare(self::NORMAL_QUEUE,false,false,false,false,false,$arguments);
         $channel->queue_declare(self::DEAD_QUEUE,false,false,false,false,false,$arguments);
