62adad30683a68d73756cb73853312ba91da39d6
lzh4yop <1131559748@qq.com>
2022-04-03 17:44:36 +0800

    86bff69cd7c8343da1412eddee95d06f

--- lar-demo/app/Console/Commands/Night/Worker.php
+++ lar-demo/app/Console/Commands/Night/Worker.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Night;
 use App\Service\RabbitMQ;
 use App\Service\TtlQueueConfig;
 use Illuminate\Console\Command;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class Worker extends Command
 {
@@ -40,8 +41,31 @@ class Worker extends Command
     public function handle()
     {
 
-        $config = new TtlQueueConfig();
-        $config->queueD();
+        //消息被拒，ttl过期，到达最大长度，成为死信
+        $channel = RabbitMQ::getChannel();
+        $callback = function (AMQPMessage $message) {
+            $this->getOutput()->writeln(date("Y-m-d H:i:s")." 收到：{$message->getBody()}");
+        };
+
+
+
+        $this->getOutput()->writeln("[*]等待接收消息");
+        /**
+         * @param string $queue 队列名
+         * @param string $consumer_tag 标签
+         * @param bool $no_local
+         * @param bool $no_ack 自动ack
+         * @param bool $exclusive 排他
+         * @param bool $nowait 异步
+         * @param callable|null $callback 回调
+         * @param int|null $ticket
+         */
+        $channel->basic_consume(TtlQueueConfig::DEAD_LETTER_QUEUE,"",false,true,false,false,$callback);
+
+        while ($channel->is_open())
+        {
+            $channel->wait();
+        }
 
         return 0;
     }
