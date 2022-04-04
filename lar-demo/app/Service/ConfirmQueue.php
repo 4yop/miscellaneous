a8b00e5612ac7b230b40090124e3ab92da6d1dce
lzh4yop <1131559748@qq.com>
2022-04-05 00:44:15 +0800

    e005750ba22cf2c9fc6f5774704a515a

--- lar-demo/app/Service/ConfirmQueue.php
+++ lar-demo/app/Service/ConfirmQueue.php
@@ -7,6 +7,13 @@ namespace App\Service;
 class ConfirmQueue
 {
     private $channel;
+
+    const CONFIRM_EXCHANGE = "confirm.exchange";
+    const BACKUP_EXCHANGE = "backup.exchange";
+
+    const CONFIRM_QUEUE = "";
+    const WARING_QUEUE = "";
+
     public function __construct()
     {
         $this->channel = RabbitMQ::getChannel();
