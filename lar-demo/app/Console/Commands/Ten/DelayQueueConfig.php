8eb6e57ab39319d57a04400c9ec497c958140565
lzh4yop <1131559748@qq.com>
2022-04-04 17:02:59 +0800

    1c27da043a00480a02aff67b8b8b4b9a

--- lar-demo/app/Console/Commands/Ten/DelayQueueConfig.php
+++ lar-demo/app/Console/Commands/Ten/DelayQueueConfig.php
@@ -4,6 +4,8 @@
 namespace App\Console\Commands\Ten;
 
 
+use App\Service\RabbitMQ;
+
 class DelayQueueConfig
 {
     const DELAYED_QUEUE_NAME = "delayed.queue";
@@ -11,5 +13,12 @@ class DelayQueueConfig
     const DELAYED_EXCHANGE_NAME = "delayed.exchange";
 
     const DELAYED_ROUTING_KEY = "delayed.routingkey";
+
+    private $channel;
+
+    public function __construct()
+    {
+        $this->channel = RabbitMQ::getChannel();
+    }
 
 }
