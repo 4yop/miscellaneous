d547e2eb3bdb98c2e27434c5f816ba0ff9a79e04
lzh4yop <1131559748@qq.com>
2022-04-04 16:58:56 +0800

    fa182cb45d0d4534007a2f0571698d92

---
+++ lar-demo/app/Console/Commands/Ten/DelayQueueConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Console\Commands\Ten;
+
+
+class DelayQueueConfig
+{
+    const DELAYED_QUEUE_NAME = "delayed.queue";
+
+    const DELAYED_EXCHANGE_NAME = "delayed.exchange";
+
+    const DELAYED_ROUTING_KEY = "delayed.routing_key";
+
+}
