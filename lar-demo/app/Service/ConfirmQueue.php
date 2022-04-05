1b6c8bb6bc77451f905e32904ec0253cc224ac93
lzh4yop <1131559748@qq.com>
2022-04-05 17:00:08 +0800

    4ea5bcef3d8ed8e0c49ca6747c110a14

--- lar-demo/app/Service/ConfirmQueue.php
+++ lar-demo/app/Service/ConfirmQueue.php
@@ -169,19 +169,19 @@ class ConfirmQueue
 
     public function sendMsg(string $msg_body = '')
     {
+        if ($this->is_confirm_select) {
 
+            //要写 wait_for_pending_acks();
+            $this->channel->set_ack_handler(function (AMQPMessage $message) {
+                // code when message is confirmed
+                echo "接收了:{$message->getBody()}\n";
+            });
 
-        //要写 wait_for_pending_acks();
-        $this->channel->set_ack_handler(function (AMQPMessage $message){
-            // code when message is confirmed
-            echo "接收了:{$message->getBody()}\n";
-        });
-
-        $this->channel->set_nack_handler(function (AMQPMessage $message){
-            // code when message is nack-ed
-            echo "没接收:{$message->getBody()}\n";;
-        });
-
+            $this->channel->set_nack_handler(function (AMQPMessage $message) {
+                // code when message is nack-ed
+                echo "没接收:{$message->getBody()}\n";;
+            });
+        }
         $properties = [
             'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
         ];
