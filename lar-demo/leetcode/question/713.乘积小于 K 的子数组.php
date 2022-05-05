73a131f5cadb64981eb52439863c5534bcbe30a2
lzh <1131559748@qq.com>
2022-05-05 10:30:41 +0800

    3c9f2092a02fc30e3b7cf8ad37414052

--- lar-demo/leetcode/question/713.乘积小于 K 的子数组.php
+++ lar-demo/leetcode/question/713.乘积小于 K 的子数组.php
@@ -7,4 +7,27 @@ class Solution {
      * @return Integer
      */
     function numSubarrayProductLessThanK($nums, $k) {
-        $
+        $res = 0;
+        foreach ($nums as $key=>$num)
+        {
+            $product = 1;
+            $j = $key;
+            while ($j < count($nums))
+            {
+                $product = $product*$nums[$j];
+                if ($product >= $k)
+                {
+                    break;
+                }
+                $res++;
+                $j++;
+            }
+        }
+        return $res;
+    }
+}
+
+$s = new Solution();
+$nums =  [10,5,2,6];
+$k = 100;
+$s->numSubarrayProductLessThanK($nums, $k);
